dbeda29f17dd60e652a9871964f8b7b688d14da8
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-10-06 20:25:07 +0000

    counter gets red when exceed the number of characters

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,14 +2,18 @@ $(document).ready(function() {
   // --- our code goes here ---
 // change event: it triggers *after* the change is made;
 // keydown event: updates are delayed by one key
-// keyup event: 
+// keyup event:
 // blur event: it triggers when we *lose focus*
-// keypress event: 
-  $("#tweet-text").bind("keyup", function (event) {
+// keypress event:
+  $("#tweet-text").bind("keyup", function(event) {
     const value = 140 - $("#tweet-text").val().length;
-    $(this).parent().find(".counter").text(value)
+    $(this).parent().find(".counter").text(value);
+    if (value < 0) {
+      $(".counter").addClass("negative-value");
+    } else {
+      $(".counter").removeClass("negative-value");
+    }
 
-
-  })
+  });
 
 });

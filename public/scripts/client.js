920e08d489a1dc564b73a5a7b29a808767611557
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-10-07 00:52:40 +0000

    createTweetElement and renderTweets functions defined and working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,76 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+
+$(document).ready(() => {
+console.log('anything')
+  const createTweetElement = (tweetObj) => {
+    // calculate how many days after the data of creation we are
+    const date = Math.round((Date.now() - tweetObj.created_at) / (1000 * 60 * 60 * 24));
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <div>
+            <img src="${tweetObj.user.avatars}">
+            <p>${tweetObj.user.name}</p>
+          </div>
+          <a href="#">${tweetObj.user.handle}</a>
+        </header>
+        <p>${tweetObj.content.text}</p>
+        <footer>
+          <p>${date} days ago</p>
+          <div>
+            <span>&#9873</span>
+            <span>&#8633</span>
+            <span>&#9829</span>
+          </div>
+        </footer>
+      </article>
+    `)
+    return $tweet;
+  };
+  const renderTweets = tweetsArray => {
+    // loops through tweets
+    tweetsArray.forEach(elem => {
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(elem);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append($tweet);
+      
+    })
+  
+  };
+  renderTweets(data);
+});
+
+
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 // keyup event:
 // blur event: it triggers when we *lose focus*
 // keypress event:
-  $("#tweet-text").bind("keyup", function(event) {
+  $("#tweet-text").bind("keyup", function() {
     const value = 140 - $("#tweet-text").val().length;
     $(this).parent().find(".counter").text(value);
     if (value < 0) {

d53c8d35a6b0f0da58fe7148bef4c268e3ea8b25
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-10-08 00:01:25 +0000

    Make the form slide up or down when the (new) Compose button is clicked

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,18 @@
 
 
 $(document).ready(() => {
+  $("#compose-button").on('click', (evt) => {
+    //evt.preventDefault();
+    // if the form is not visible, slide down the form
+    if ($('.new-tweet:visible').length === 0) {
+      $('.new-tweet').slideDown();
+      return $("#tweet-text").focus();
+    }
+    // else, unfocus the button and hide the form
+    $("#compose-button").blur()
+    return $('.new-tweet').slideUp();
+  })
+
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
@@ -69,7 +81,7 @@ $(document).ready(() => {
     }
     //The user should be given an error that their tweet is empty
     if (!$("#tweet-text").val()) {
-      $("#error-message").html("&#128680; Your tweet seems to be empty. &#128680;");
+      $("#error-message").html("&#128680; Your tweet seems to be empty. &#128680 ");
       $("#error-message").slideDown({
         start: function () {
           $(this).css({
